89b66b156f619829f142fefcee961caefc2c6963
jiheekim <jhkim@youngjintech.com>
2025-01-29 20:49:43 +0900

    그리드 레이아웃 적용

--- src/view/Dashboard.jsx
+++ src/view/Dashboard.jsx
@@ -1,51 +1,51 @@
 import Grid from '@mui/material/Grid2';
 import {styled} from "@mui/material";
+import {darkTheme} from "../theme.js";
 
-const Skeleton = styled('div')(({ theme, height }) => ({
-    backgroundColor: theme.palette.action.hover,
+const Skeleton = styled('div')(({theme, height}) => ({
+
     borderRadius: theme.shape.borderRadius,
     height,
     content: '" "',
 }));
-function Dashboard(){
-    return(
+
+function Dashboard() {
+    return (
         <div>
             <Grid container spacing={1}>
-                <Grid size={12} />
-                <Grid size={12}>
-                    <Skeleton height={14} />
-                </Grid>
-                <Grid size={12}>
-                    <Skeleton height={14} />
-                </Grid>
-                <Grid size={4}>
-                    <Skeleton height={100} />
-                </Grid>
-                <Grid size={8}>
-                    <Skeleton height={100} />
+                <Grid size={{xs: 12}}>
+                    <Skeleton height={40} sx={{
+                        backgroundColor: darkTheme.palette.action.hover,
+                    }}/>
                 </Grid>
 
-                <Grid size={12}>
-                    <Skeleton height={150} />
+                <Grid size={{xs: 12, md: 9}}>
+                    <Grid size={12}>
+                        <Skeleton height={280}></Skeleton>
+                    </Grid>
+                    <Grid size={12}>
+                        <Skeleton height={220} sx={{
+                            marginTop: 1.5,
+                            backgroundColor: darkTheme.palette.action.hover,
+                        }}></Skeleton>
+                    </Grid>
+                    <Grid size={12}>
+                        <Skeleton height={280} sx={{
+                            marginTop: 1.5,
+                            backgroundColor: darkTheme.palette.action.hover,
+                        }}></Skeleton>
+                    </Grid>
                 </Grid>
-                <Grid size={12}>
-                    <Skeleton height={14} />
+                <Grid size={{xs: 12, md: 3}}>
+                    <Skeleton height={805} sx={{
+                        backgroundColor: darkTheme.palette.action.hover,
+                        marginTop: {xs: 0.5, md: 0}
+                    }}/>
                 </Grid>
 
-                <Grid size={3}>
-                    <Skeleton height={100} />
-                </Grid>
-                <Grid size={3}>
-                    <Skeleton height={100} />
-                </Grid>
-                <Grid size={3}>
-                    <Skeleton height={100} />
-                </Grid>
-                <Grid size={3}>
-                    <Skeleton height={100} />
-                </Grid>
             </Grid>
         </div>
     )
 }
+
 export default Dashboard;
