53e156a091867b131fda2279efc05fead961f65b
jiheekim <jhkim@youngjintech.com>
2025-02-01 21:20:28 +0900

    TODAY'S FORECAST 예제 UI 기능 적용

--- src/components/dashboard/TodayTimeBox.jsx
+++ src/components/dashboard/TodayTimeBox.jsx
@@ -1,11 +1,24 @@
 import Grid from "@mui/material/Grid";
 import {Skeleton} from "../../layout/Layout.jsx";
-import {useReducer, useRef, useState} from "react";
+import {useRef, useState} from "react";
 import {Box} from "@mui/material";
+import {darkTheme} from "../../theme.js";
 
 //icon
 import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
 import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+
+import sunny from '../../assets/images/sunny.png';
+import little_cloud from '../../assets/images/little_cloud.png';
+import many_cloud from '../../assets/images/many_cloud.png';
+import cloud from '../../assets/images/cloud.png';
+import rain from '../../assets/images/rain.png';
+import rain_snow from '../../assets/images/rain_snow.png';
+import snow from '../../assets/images/snow.png';
+import thunder from '../../assets/images/thunder.png';
+import fog from '../../assets/images/fog.png';
+
+
 
 
 export const TodayTimeBox = () => {
@@ -50,7 +63,7 @@ export const TodayTimeBox = () => {
                         whiteSpace: "nowrap",
                         display: 'flex',
                         scrollBehavior: "smooth",
-                        "&::-webkit-scrollbar": { display: "none" },
+                        "&::-webkit-scrollbar": {display: "none"},
                         "-ms-overflow-style": "none",
                         "scrollbar-width": "none",
                     }}
@@ -62,11 +75,53 @@ export const TodayTimeBox = () => {
                                     key={index}
                                     height={160}
                                     sx={{
-                                        backgroundColor: 'pink',
                                         width: 120,
                                         ml: 0.5,
-                                        mr: 0.5
-                                    }}/>
+                                        mr: 0.5,
+                                        mt: 1,
+                                        borderRadius: 0,
+                                        borderRight: `1px solid ${darkTheme.palette.text.trans}`,
+                                    }}>
+                                    <Grid container spacing={1}>
+                                        <Grid item xs={12}>
+                                            <Skeleton height={30} sx={{
+                                                // backgroundColor: darkTheme.palette.action.hover,
+                                                color: darkTheme.palette.text.trans,
+                                                padding: 1,
+                                                fontSize: 11,
+                                                textAlign: 'center'
+                                            }}>
+                                                AM {index+1}:00
+                                            </Skeleton>
+                                        </Grid>
+
+                                        <Grid item xs={12}>
+                                            <Skeleton height={60} sx={{textAlign: 'center'}}>
+                                                <img
+                                                    src={snow}
+                                                    alt="sunny"
+                                                    style={{
+                                                        width: '40%',
+                                                        marginTop: 5
+                                                    }}
+                                                />
+                                            </Skeleton>
+                                        </Grid>
+
+                                        <Grid item xs={12}>
+                                            <Skeleton height={45} sx={{
+                                                color: darkTheme.palette.text.primary,
+                                                padding: 1,
+                                                fontSize: 20,
+                                                textAlign: 'center'
+                                            }}>
+                                                -3℃
+                                            </Skeleton>
+
+                                        </Grid>
+                                    </Grid>
+
+                                </Skeleton>
                             </Box>
                         ))
                     }
